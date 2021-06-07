445f4eb58cf772c7419b489a5370bb6db30e50bf
omunoz <steven.120189@gmail.com>
2021-06-07 11:09:53 -0400

    added signup route

--- models/index.js
+++ models/index.js
@@ -1,1 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+
+
+const db = mongoose.connection;
+
+//Setup event fro db to print connection
+db.once('open', () => {
+    console.log(`Connect to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on('error', err => console.log (`Database error`, err));
+
+//Import all models
+
+const User = require('./User');
+
+
+//Export all models from file
+module.exports = {
+    User
+}
